6efcdafbf30e690b4a095f8abfa058feae24a4c2
Akhilesh Kumar <akhilesh_k@outlook.com>
2021-02-23 14:13:31 +0530

    implemented multithreading

--- src/main/java/com/example/searchappv1/service/impl/SearchServiceImpl.java
+++ src/main/java/com/example/searchappv1/service/impl/SearchServiceImpl.java
@@ -10,52 +10,79 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static javax.swing.UIManager.get;
 
 @Service
 public class SearchServiceImpl implements SearchService {
+	public void awaitTerminationAfterShutdown(ExecutorService threadPool) {
+		threadPool.shutdown();
+		try {
+			if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+				threadPool.shutdownNow();
+			}
+		} catch (InterruptedException ex) {
+			threadPool.shutdownNow();
+			Thread.currentThread().interrupt();
+		}
+	}
 	@Autowired
 	private SearchClient searchClient;
+
 	@Override
 	public SearchResponseDTO getProducts(SearchRequestDTO request ){
-		Map<String, Object> productResponse=searchClient.getProducts(request.getSearchTerm());
-		Map<String, Object> locationBaseProductsResponse=searchClient.getProducts("stockLocation:"+"\""+request.getStockLocation()+"\"");
 
-		List<Map<String,Object>> products=(List<Map<String,Object>>)((Map<String,Object>)productResponse.get("response")).get("docs");
-		List<Map<String,Object>> locationBaseProducts=(List<Map<String,Object>>)((Map<String,Object>)locationBaseProductsResponse.get("response")).get("docs");
-
-		List<ProductDTO>productDTOList=new ArrayList<>();
-		for(Map<String,Object> product1:products){
-			ProductDTO product=new ProductDTO();
-			product.setDescription((String) product1.get("description"));
-			product.setTitle((String)product1.get("nameSearch"));
-			product.setSalePrice((Double) product1.get("salePrice"));
-			product.setInStock((int) product1.get("isInStock") == 1? true:false);
-
-			productDTOList.add(product);
-		}
-
-		List<ProductDTO>locationDTOList=new ArrayList<>();
-		for(Map<String,Object> product2:locationBaseProducts){
-			ProductDTO locationBaseProduct=new ProductDTO();
-			locationBaseProduct.setDescription((String) product2.get("description"));
-			locationBaseProduct.setTitle((String)product2.get("nameSearch"));
-			locationBaseProduct.setSalePrice((Double) product2.get("salePrice"));
-			locationBaseProduct.setInStock((int) product2.get("isInStock") == 1? true:false);
-
-			locationDTOList.add(locationBaseProduct);
-		}
-
-
+		ExecutorService executor = Executors.newFixedThreadPool(2);
 		SearchResponseDTO searchResponseDTO =new SearchResponseDTO();
-		searchResponseDTO.setProducts(productDTOList);
-		searchResponseDTO.setLocationBaseProducts(locationDTOList);
+
+		Runnable searchTermTask = () -> {
+			Map<String, Object> productResponse=searchClient.getProducts(request.getSearchTerm());
+			List<Map<String,Object>> products=(List<Map<String,Object>>)((Map<String,Object>)productResponse.get("response")).get("docs");
+
+			List<ProductDTO>productDTOList=new ArrayList<>();
+			for(Map<String,Object> product1:products){
+				ProductDTO product=new ProductDTO();
+				product.setDescription((String) product1.get("description"));
+				product.setTitle((String)product1.get("nameSearch"));
+				product.setSalePrice((Double) product1.get("salePrice"));
+				product.setInStock((int) product1.get("isInStock") == 1? true:false);
+
+				productDTOList.add(product);
+				searchResponseDTO.setProducts(productDTOList);
+			}
+		};
+
+		Runnable locationSearchTask = () -> {
+
+				Map<String, Object> locationBaseProductsResponse=searchClient.getProducts("stockLocation:"+"\""+request.getStockLocation()+"\"");
+				List<Map<String,Object>> locationBaseProducts=(List<Map<String,Object>>)((Map<String,Object>)locationBaseProductsResponse.get("response")).get("docs");
+				List<ProductDTO>locationDTOList=new ArrayList<>();
+				for(Map<String,Object> product2:locationBaseProducts){
+					ProductDTO locationBaseProduct=new ProductDTO();
+					locationBaseProduct.setDescription((String) product2.get("description"));
+					locationBaseProduct.setTitle((String)product2.get("nameSearch"));
+					locationBaseProduct.setSalePrice((Double) product2.get("salePrice"));
+					locationBaseProduct.setInStock((int) product2.get("isInStock") == 1? true:false);
+
+					locationDTOList.add(locationBaseProduct);
+				}
+				searchResponseDTO.setLocationBaseProducts(locationDTOList);
+		};
+
+		executor.execute(searchTermTask);
+		executor.execute(locationSearchTask);
+
+		awaitTerminationAfterShutdown(executor);
+
 		return searchResponseDTO;
 
 //		SearchResponseDTO responseDTO=new SearchResponseDTO();
 //		ProductDTO product=new ProductDTO();
-//		product.setDescription("Samsung Galaxy S5... Blah blahhh...");
+//		product.setDescription("Samsung Galaxy S5... Blah blah...");
 //		product.setTitle("Samsung Galaxy S5");
 //		product.setInStock(true);
 //		product.setSalePrice(2000);
