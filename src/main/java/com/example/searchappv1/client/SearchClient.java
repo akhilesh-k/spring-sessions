af055e4caf20ddedb07b061f052fc4ac55834c3a
Akhilesh Kumar <akhilesh_k@outlook.com>
2021-02-23 10:18:28 +0530

    solr search implemented.

---
+++ src/main/java/com/example/searchappv1/client/SearchClient.java
@@ -0,0 +1,17 @@
+package com.example.searchappv1.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+//	http://10.177.68.40:8983/solr/productCollection/browse?q=Samsung
+@FeignClient (name = "search-client",url = "http://10.177.68.40:8983")
+public interface SearchClient {
+@RequestMapping(method = RequestMethod.GET,path = "/solr/productCollection/select")
+	public Map<String, Object> getProducts(@RequestParam("q") String query);
+
+
+}
---
+++ src/main/java/com/example/searchappv1/dto/SearchRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.searchappv1.dto;
+
+public class SearchRequestDTO {
+	private String searchTerm;
+	public String getSearchTerm(){
+		return searchTerm;
+	}
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
+	}
+}
